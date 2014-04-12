02ba627a455b7b72d3b44aa958703b788d34cf27
funkybob <curtis@tinbrain.net>
2014-04-12 13:55:10 +1000

    Start implementing tag compilation support

--- rattle/template.py
+++ rattle/template.py
@@ -12,6 +12,10 @@ class Template(object):
     def __init__(self, source):
         self.source = source
 
+        # A list of compiled tags
+        self.compiled_tags = []
+        self.filter_functions = {}
+
         self.lexer = lg.build()
         self.parser = pg.build()
 
@@ -29,7 +33,27 @@ class Template(object):
             code = self.parser.parse(self.lexer.lex(token.content))
         elif token.mode == TOKEN_BLOCK:
             # Parse args/kwargs
-            pass
+            parsed = self.token_parser.parse(self.lexer.lex(token.content))
+            token = parsed.pop(0)
+            args = []
+            kwargs = []
+            for arg in all_args:
+                if isinstance(arg, ast.keyword):
+                    kwargs.append(arg)
+                else:
+                    args.append(arg)
+            tag = self.tags[tag_name](self, *args, **kwargs)
+            # place tag code in local "compiled_tags" storage
+            self.compiled_tags.append(tag)
+            code = ast.Call(
+                func=ast.Subscript(
+                    value=ast.Name(id='compiled_tags', ctx=ast.Load()),
+                    slice=ast.Index(value=ast.Num(n=len(self.compiled_tags)-1), ctx=ast.Load()),
+                    ctx=ast.Load(),
+                ),
+                args=args,
+                keywords=kwargs,
+            )
         else:
             # Must be a comment
             pass
@@ -40,8 +64,8 @@ class Template(object):
     def parse(self):
         '''Convert the parsed tokens into a list of expressions
         Then join them'''
-        steps = []
         self.stream = tokenise(self.source)
+        steps = []
         for token in self.stream:
             code = self._token_to_code(token)
             if code:
@@ -70,5 +94,7 @@ class Template(object):
     def render(self, context={}):
         return u''.join(eval(self.func, {}, {
             'context': context,
+            'compiled_tags': self.compiled_tags,
+            'filters': self.filter_functions,
         }))
 
